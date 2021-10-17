c6850b18f8618ace47e878a66473d8a24b0681db
xTheCoderx <justaguyoney@gmail.com>
2021-10-18 09:26:20 +1300

    Added card flip animation.

--- app/src/main/java/com/realtomjoney/card_flip_animation/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/card_flip_animation/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.realtomjoney.card_flip_animation
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import com.realtomjoney.card_flip_animation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +15,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setBindings()
+
+        binding.textView.setOnClickListener {
+            val animation1 = ObjectAnimator.ofFloat(binding.textView, "scaleX", 1f, 0f)
+            val animation2 = ObjectAnimator.ofFloat(binding.textView, "scaleX", 0f, 1f)
+
+            animation1.interpolator = DecelerateInterpolator()
+            animation2.interpolator = AccelerateInterpolator()
+
+            animation1.addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    binding.textView.text = "Back"
+                    animation2.start()
+                }
+            })
+
+            animation1.start()
+        }
     }
 
     private fun setBindings() {
